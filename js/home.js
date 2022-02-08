fe10a97a0f80a5da94c279264e1b18b759ce9a0b
Brandon Gompert <brandongompert@gmail.com>
2022-02-08 18:49:53 -0500

    added random quotes to main page with fade effect

---
+++ js/home.js
@@ -0,0 +1,24 @@
+//RANDOM QUOTE GENERATOR
+let quote = document.getElementsByClassName("quote-generator");
+let randomChoice;
+let jsonData = fetch("https://type.fit/api/quotes");
+//function that returns random quote into each quote-generator class element
+for (let i = 0; i <= quote.length; i++) {
+  jsonData = fetch("https://type.fit/api/quotes")
+    .then((response) => response.json())
+    .then((data) => (randomChoice = choose(data)))
+    .then(
+      () =>
+        (quote[i].innerHTML =
+          randomChoice.text +
+          `<br> -` +
+          //replaces "Null" with "Unknown" for semantic purposes
+          (randomChoice.author ? randomChoice.author : "Unknown"))
+    );
+}
+//random choice function ** used to randomly generate a quote from quote JSON data **
+function choose(choices) {
+  jsonData = choices;
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
